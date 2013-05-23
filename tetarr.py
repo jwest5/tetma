7b3a87c87563d11da7a4b2396c2c6c61dfc785d5
Joe West <jwest5@trinity.edu>
2013-05-23 04:22:29 -0500

    fixed Tetarr.__new__(L,data) so type(data) can be Tetarr

--- tetarr.py
+++ tetarr.py
@@ -8,16 +8,16 @@ import numpy as np
 
 class Tetarr(np.ndarray):
     """"""
-    def __new__(cls,L=100,dtype=int,data=None):
+    def __new__(cls,L=100,data=None):
         """"""
         if data == None:
             input_array = np.zeros((47,L))
-        elif type(data) == np.ndarray:
-            input_array = data
+        elif hasattr(data,'shape'):
+            input_array = data.copy()
         obj = np.asarray(input_array).view(cls)
         obj.germline = obj[:2]
         obj.somatic = obj[2:]
-        obj.L = obj.size[1]
+        obj.L = obj.shape[1]
         return obj
         
     def __array_finalize__(self, obj):
@@ -39,4 +39,22 @@ class Tetarr(np.ndarray):
         for i in range(self.L):
             new_tet.somatic[:,i] = np.random.choice(dup_somatic[:,i],45,False)
         return new_tet
+        
+    def fitness(self,s=0.2,h=0.5):
+        """"""
+        deleterious = (self.somatic == -1)
+        beneficial = (self.somatic == 1)
+        d = sum((np.mean(deleterious,axis=0)**(np.log(h)/np.log(0.5)))*s)
+        b = sum((np.mean(beneficial,axis=0)**(np.log(h)/np.log(0.5)))*s)
+        w = 1 - d + b
+        return w
+        
+    def genomic_exclusion(self):
+        """"""
+        which_allele = np.random.binomial(1,0.5,self.L) == 0
+        #hap_germ = np.where(which_allele,self.germline[0,:],self.germline[1,:])
+        hap_germ = which_allele.choose([self[0],self[1]],np.zeros((1,self.L)))        
+        homozygous = np.repeat(hap_germ,47,axis=0)
+        homozygote = Tetarr(data=homozygous)
+        return homozygote
         
