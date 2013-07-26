8c254aedd087fe850cc595c11f4baff946c3c3d6
Joe West <jwest5@trinity.edu>
2013-07-26 16:35:51 -0500

    fixed mutate and added cross, sort of fitness...

--- newTet.py
+++ newTet.py
@@ -18,9 +18,9 @@ def mutational_effects(n, s, beta):
     s -- mean effect of a mutation
     beta -- shape parameter of the gamma distribution (inf indicates equal effects)
     """
-    if sign(s) == 1:
+    if np.sign(s) == 1:
         beneficial = True
-    elif sign(s) == -1:
+    elif np.sign(s) == -1:
         beneficial = False
     else:
         return "Invalid s: must be nonzero."
@@ -38,7 +38,6 @@ def mutational_effects(n, s, beta):
         return "Invalid beta: must be 0 < beta < inf."
 
 class Tet:
-    
     """
     Represent individual organism.
     
@@ -46,9 +45,7 @@ class Tet:
     L - Number of loci.
     germline - 2xL array representing the diploid germline genome.
     somatic - 45XL array representing the somatic genome.
-    
     """
-    
     def __init__(self, L=100, germline=None, somatic=None):
         """
         Initialize a Tet object.
@@ -77,25 +74,18 @@ class Tet:
         s = "Germline:\n" + str(self.germline) + "\nSomatic:\n" + str(self.somatic)
         return s
         
-    def mutate_germline(self,mu,value):
-        """Mutates each allele at each locus in the germline to the given value
-        with probability mu. Uses numpy.random.binomial to determine which
-        alleles mutate, then uses a boolean array to assign the value those
-        alleles. Right now, mutations are irreversible...
-        (currently using value = 0 for wild-type, value = -1 for deleterious
-        mutations, and value = 1 for beneficial mutations)."""
-        mutants = (np.random.binomial(1,mu,(47,self.L)) == 1) & (self.germline == 0)
-        self.germline[mutants] = value
-        
-    def mutate_somatic(self,mu,value):
-        """Mutates each allele at each locus in the somatic to the given value
-        with probability mu. Uses numpy.random.binomial to determine which
-        alleles mutate, then uses a boolean array to assign the value those
-        alleles. Right now, mutations are irreversible...
-        (currently using value = 0 for wild-type, value = -1 for deleterious
-        mutations, and value = 1 for beneficial mutations)."""
-        mutants = (np.random.binomial(1,mu,(47,self.L)) == 1) & (self.somatic == 0)
-        self.somatic[mutants] = value
+    def mutate(self,mu,s,beta,genome='both'):
+        """"""
+        if genome is 'somatic' or 'both':
+            mutants = (np.random.binomial(1,mu,(45,self.L)) == 1) & (self.somatic == 0)
+            n = sum(mutants)
+            effects = mutational_effects(n,s,beta)
+            self.somatic[mutants] = effects
+        if genome is 'germline' or 'both':
+            mutants = (np.random.binomial(1,mu,(2,self.L)) == 1) & (self.germline == 0)
+            n = sum(mutants)
+            effects = mutational_effects(n,s,beta)
+            self.germline[mutants] = effects
         
     def reproduce(self):
         """Returns a new Tet object. The germline genome of the new individual
@@ -111,7 +101,61 @@ class Tet:
         new_tet.germline = self.germline
         return new_tet
         
-    def fitness(self,s,h):
+    def fitness(self):
+        """meh, it'll do for the moment... also, ignores dominance..."""
+        w = sum(np.mean(self.somatic,axis=0))
+        return w
+        
+    def genomic_exclusion(self):
+        """Generates a haploid genotype from the germline, then generates an
+        full homozygote. The resulting individual can be used to assess the
+        fitness of the germline."""
+        which_allele = np.random.binomial(1,0.5,self.L) == 0
+        hap_germ = np.where(which_allele,self.germline[0,:],self.germline[1,:])
+        homozyg_germ = np.zeros((2,self.L))
+        homozyg_soma = np.zeros((45,self.L))
+        homozyg_germ[0:2,:] = hap_germ
+        homozyg_soma[0:45,:] = hap_germ
+        GE_tet = Tet(self.L,homozyg_germ,homozyg_soma)
+        return GE_tet
+        
+    def cross(self,other):
+        """"""
+        s_alleles = np.random.binomial(1,0.5,self.L) == 0
+        s_germ = np.where(s_alleles,self.germline[0,:],self.germline[1,:])
+        o_alleles = np.random.binomial(1,0.5,other.L) == 0
+        o_germ = np.where(o_alleles,other.germline[0,:],other.germline[1,:])
+        new_germ = np.zeros((2,self.L))
+        new_germ[0,:] = s_germ
+        new_germ[1,:] = o_germ
+        new_soma = np.zeros((45,self.L))
+        new_soma[0:23,:] = s_germ
+        new_soma[23:45,:] = o_germ
+        progeny = Tet(self.L, new_germ, new_soma)
+        return progeny
+        
+    def mutate_germline(self,mu,values):
+        """Mutates each allele at each locus in the germline to the given value
+        with probability mu. Uses numpy.random.binomial to determine which
+        alleles mutate, then uses a boolean array to assign the value those
+        alleles. Right now, mutations are irreversible...
+        (currently using value = 0 for wild-type, value = -1 for deleterious
+        mutations, and value = 1 for beneficial mutations)."""
+        mutants = (np.random.binomial(1,mu,(2,self.L)) == 1) & (self.germline == 0)
+        self.germline[mutants] = values
+        
+    def mutate_somatic(self,mu,values):
+        """Mutates each allele at each locus in the somatic to the given value
+        with probability mu. Uses numpy.random.binomial to determine which
+        alleles mutate, then uses a boolean array to assign the value those
+        alleles. Right now, mutations are irreversible...
+        (currently using value = 0 for wild-type, value = -1 for deleterious
+        mutations, and value = 1 for beneficial mutations)."""
+        mutants = (np.random.binomial(1,mu,(45,self.L)) == 1) & (self.somatic == 0)
+        self.somatic[mutants] = values
+        
+    def fitness_old(self,s,h):
+        # Doesn't work for new mutate method...
         """Calculates additive fitness..."""
         deleterious = (self.somatic == -1)
         beneficial = (self.somatic == 1)
@@ -120,10 +164,6 @@ class Tet:
         w = 1 - d + b
         return w
         
-    def genomic_exclusion(self):
-        """"""
-        
-    
         
         
         
